6a9c7c15be3eaf66e39fe8942c392d008a6a8728
iliketrains2314 <aaron.tran890@gmail.com>
2025-05-14 20:20:34 -0700

    Update test.v

--- test.v
+++ test.v
@@ -1,1 +1,35 @@
+always@(posedge clk)
+  begin
+    next_state=state;
+    case(state)
+      S0:
+        begin
+          if(in)
+            next_state=S1;
+          else
+            next_state=S2;
+        end
 
+      S1:
+        begin
+          if(in)
+            next_state=S3;
+          else
+            next_state=S0;
+        end
+
+      S2:
+        begin
+          if(in)
+            next_state=S0;
+          else
+            next_state=S3;
+        end
+
+      S3:
+        begin
+            next_state=S1;
+
+        end
+    endcase
+  end
